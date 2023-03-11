28c19a0b6213d21009a87ced12ebc7c031c65d4b
Ramon Brito <git@ramondsb.com>
2023-03-11 21:31:42 +0000

    Add unit tests

---
+++ src/test/scala/ttt/MainSpec.scala
@@ -0,0 +1,39 @@
+package ttt
+
+import ttt.Main.{Board, CellType, Empty, Finished, Human, Machine, O, X, checkGameState}
+
+class MainSpec extends munit.FunSuite {
+    val players = Seq(Human(X), Machine(O))
+    test("row victory") {
+      val board: Board = Array[CellType](
+        X, X, X,
+        O, X, O,
+        Empty, Empty, Empty
+      )
+      val obtained = checkGameState(board, players)
+      val expected = Finished(Some(Human(X)))
+      assertEquals(obtained, expected)
+    }
+
+    test("column victory") {
+      val board: Board = Array[CellType](
+        O, X, X,
+        O, X, O,
+        O, Empty, Empty
+      )
+      val obtained = checkGameState(board, players)
+      val expected = Finished(Some(Machine(O)))
+      assertEquals(obtained, expected)
+    }
+
+    test("Draw") {
+      val board: Board = Array[CellType](
+        O, X, O,
+        X, O, X,
+        X, O, X
+      )
+      val obtained = checkGameState(board, players)
+      val expected = Finished(None)
+      assertEquals(obtained, expected)
+    }
+}
